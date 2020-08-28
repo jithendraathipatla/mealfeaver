cc505fa28291a3ffa256e196a03b8af2c5eb230c
jithendraathipatla <37688287+jithendraathipatla@users.noreply.github.com>
2020-08-28 16:49:21 +0530

    Adding a banner image and text

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,40 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import ModalComponant from '../components/modal'
+import ModalComponant from "../components/modal"
 import { css } from "@emotion/core"
 import Categories from "../components/categories"
-
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 import BusinessFormSignup from "../components/BusinessPatnerform"
-import DeliveryFormSignup from '../components/DeliveryForm'
+import DeliveryFormSignup from "../components/DeliveryForm"
 
 const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "top_bg.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 1500, maxHeight: 450) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
   return (
     <Layout>
-      <SEO title="Home" />
+      <SEO title="Mealfever" />
       <div>
+        <div>
+          <div css={firstBanner}>
+            <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+          </div>
+          <div css={firstBannertText}>
+            <h1>
+              Shop From Your Favourite Store!
+            </h1>
+          </div>
+        </div>
         <Categories />
         <div css={division}></div>
         <div css={sponser}>
@@ -25,7 +47,9 @@ const IndexPage = () => {
               nec mollis dolor urna nec neque. Quisque egestas consequat
               suscipit. Cras condimentum dui vitae laoreet feugiat.
             </p>
-            <ModalComponant name="Learn More"><BusinessFormSignup/></ModalComponant>
+            <ModalComponant name="Learn More">
+              <BusinessFormSignup />
+            </ModalComponant>
           </div>
           <div>
             <img src="https://puzzlerbox.com/template-kits/food-delivery/wp-content/uploads/sites/37/elementor/thumbs/02-elements-30-food-delivery-icons-W5NQ6ZA-otks8k8vwl3ur0ri2maolpef6u6k5lljp3zmojeo6g.png" />
@@ -36,7 +60,9 @@ const IndexPage = () => {
               nec mollis dolor urna nec neque. Quisque egestas consequat
               suscipit. Cras condimentum dui vitae laoreet feugiat.
             </p>
-            <ModalComponant name="Learn More"><DeliveryFormSignup/></ModalComponant>
+            <ModalComponant name="Learn More">
+              <DeliveryFormSignup />
+            </ModalComponant>
           </div>
         </div>
       </div>
@@ -45,6 +71,25 @@ const IndexPage = () => {
 }
 
 export default IndexPage
+
+const firstBannertText = css`
+  position: absolute;
+  margin-top: -15%;
+  width: 100%;
+  padding: 0px 10%;
+  text-align: center;
+  h1 {
+    text-shadow: 0px 5px 5px rgba(255, 255, 255, 0.15);
+    color: #1d1d1d;
+    font-family: "Montserrat", Sans-serif;
+    font-size: 60px;
+    font-weight: 900;
+    line-height: 1.1em;
+  }
+`
+const firstBanner = css`
+  position: relative;
+`
 
 const sponser = css`
   transition: background 0.3s, border-radius 0.3s, opacity 0.3s;
