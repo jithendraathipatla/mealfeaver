706f6f5fd939f7264b747f03ddc83d1350fb6103
jithendraathipatla <37688287+jithendraathipatla@users.noreply.github.com>
2020-08-29 13:57:10 +0530

    second commit

--- src/template/stores.js
+++ src/template/stores.js
@@ -1,28 +1,27 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Image from "gatsby-image"
 import Layout from "../components/layout"
 
 export const query = graphql`
-
-    query MyQuery {
-        allMerchantJson {
-          nodes {
-            slug
-            title
-            price
-          }
-        }
-      }
-      
-  
+  query($slug: String!) {
+    merchantJson(slug: { eq: $slug }) {
+      title
+      price
+    }
+  }
 `
 
 const MerchPage = ({ data }) => {
-  console.log(data)
+  const merch = data.merchantJson
+
   return (
     <Layout>
-     
-     
+      <div style={{padding:"100px 30%"}}>
+      <h1>{merch.title}</h1>
+      <h2>{merch.price}</h2>
+      <div>Item List Coming soon</div>
+      </div>
     </Layout>
   )
 }
