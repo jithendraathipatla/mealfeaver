306816be0042209d3677f255ca397b1fb0213f5e
jithendraathipatla <37688287+jithendraathipatla@users.noreply.github.com>
2020-08-29 08:08:00 +0530

    making initital change to Faq and News room pages

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,16 +1,64 @@
-import React from 'react'
-import LayoutComponant from '../components/layout'
+import React from "react"
+import LayoutComponant from "../components/layout"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { css } from "@emotion/core"
+
 
 const About = () => {
-    return(
-        <LayoutComponant>
-            <div style={{padding:"200px"}}>
-            I am from News Room
-            </div>
-            
-        </LayoutComponant>
-    )
+  const data = useStaticQuery(graphql`
+  query {
+    placeholderImage: file(relativePath: { eq: "city_bg.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 1200, maxHeight: 200) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`)
+  return (
+    <LayoutComponant>
+      <br />
+      <br />
+      <div css={one}>
+        <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+        <div css={two}>
+          <h2>News Room</h2>
+          <span>Get all the updates about the happenings</span>
+        </div>
+        <br />
+        <br />
+        </div>
+    </LayoutComponant>
+  )
 }
 
+export default About
 
-export default About;
+
+const one = css`
+  img {
+    position: relative;
+  }
+`
+
+const two = css`
+  position: absolute;
+  top: 20%;
+  padding: 0px 140px;
+  h2 {
+    color: #1d1d1d;
+    font-family: "Montserrat", Sans-serif;
+    font-size: 55px;
+    font-weight: 700;
+    line-height: 1.2em;
+    letter-spacing: 0px;
+    margin-bottom: 2px;
+  }
+  span {
+    color: #7a7a7a;
+    font-weight: 700;
+    padding-left: 5px;
+  }
+`
