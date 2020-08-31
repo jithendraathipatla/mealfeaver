237476ba4d3525032747969149d1072f8be3a5bb
jithendraathipatla <37688287+jithendraathipatla@users.noreply.github.com>
2020-08-31 16:56:29 +0530

    second commit

--- src/pages/sendackages.js
+++ src/pages/sendackages.js
@@ -1,27 +1,41 @@
-import React from 'react'
-import LayoutComponent from '../components/layout'
-import {css} from '@emotion/core'
+import React,{useState} from "react"
+import LayoutComponent from "../components/layout"
+import { css } from "@emotion/core"
+import AutoPlaceSuggest from "../components/autoPlaceSuggest"
 
 const FoodDelivery = () => {
-    return(
-        <LayoutComponent>
-           <div css={mainheader}>
-               <br/>
-               <br/>
-               <br/>
-               <h3>Send Packages</h3>
-           </div>
-           <div style={{height:"400px"}}>
+    const [eventvenue, seteventvenue] = useState('')
+    let _venue = 'Bangalore';
+    const handleVenue = (value) => {
+        _venue = value;
+    };
+  return (
+    <LayoutComponent>
+      <div css={mainheader}>
+        <br />
+        <br />
+        <br />
+        <h3>Send Packages</h3>
+      </div>
+      <div style={{ height: "400px" }}>
+        <h1>PickUp</h1><AutoPlaceSuggest
+          value={eventvenue}
+          handleVenue={handleVenue}
+        />
 
-           </div>
-        </LayoutComponent>
-    )
+<h1>Drop</h1><AutoPlaceSuggest
+          value={eventvenue}
+          handleVenue={handleVenue}
+        />
+      </div>
+    </LayoutComponent>
+  )
 }
 
-export default FoodDelivery;
+export default FoodDelivery
 
 const mainheader = css`
-background: #ffa14c;
-border-top: #242424;
-padding: 15px 170px;
+  background: #ffa14c;
+  border-top: #242424;
+  padding: 15px 170px;
 `
